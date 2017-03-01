e42b6e1a6d6e2df3fd9a7f7c68ee64bd20bad42e
Nigel Price <pricen@njpenterprises.com>
2017-03-01 15:24:25 +0000

    Spfx-v1 (#1)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- src/webparts/siteInfo/SiteInfoWebPart.ts
+++ src/webparts/siteInfo/SiteInfoWebPart.ts
@@ -1,24 +1,23 @@
+import { Version } from '@microsoft/sp-core-library';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import {
   BaseClientSideWebPart,
-  IPropertyPaneSettings,
-  IWebPartContext,
+  IPropertyPaneConfiguration,
   PropertyPaneTextField,
   PropertyPaneCheckbox
-} from '@microsoft/sp-client-preview';
 
-import SiteInfo, { ISiteInfoProps } from './components/SiteInfo';
+} from '@microsoft/sp-webpart-base';
+
+import SiteInfo, {ISiteInfoProps }  from './components/SiteInfo';
+//import { IHelloWorldWebPartProps } from './ISiteInfoWebPartProps';
 import { ISiteInfoWebPartProps } from './ISiteInfoWebPartProps';
 
 export default class SiteInfoWebPart extends BaseClientSideWebPart<ISiteInfoWebPartProps> {
 
-  public constructor(context: IWebPartContext) {
-    super(context);
-  }
 
   public render(): void {
-    const element: React.ReactElement<ISiteInfoProps> = React.createElement(SiteInfo, {
+    const element: React.ReactElement<ISiteInfoWebPartProps> = React.createElement(SiteInfo, {
       description: this.properties.description,
       showLists: this.properties.showLists,
       showUser: this.properties.showUser,
@@ -28,7 +27,11 @@ export default class SiteInfoWebPart extends BaseClientSideWebPart<ISiteInfoWebP
     ReactDom.render(element, this.domElement);
   }
 
-  protected get propertyPaneSettings(): IPropertyPaneSettings {
+    protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
@@ -44,11 +47,11 @@ export default class SiteInfoWebPart extends BaseClientSideWebPart<ISiteInfoWebP
                   placeholder: "enter a description"
                 }),
                 PropertyPaneCheckbox('showLists', {
-                  isChecked: false,
+                  checked: false,
                   text: "Show Lists"
                 }),
                 PropertyPaneCheckbox('showUser', {
-                  isChecked: false,
+                  checked: false,
                   text: "Show User"
                 })
               ]
